aeb126e7b166678d856490b95a5633006c0cdd51
tanmayi-dev <tanmayi.d.99+github@gmail.com>
2023-12-10 17:55:07 +0530

    chore: fix anagram code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,27 +5,38 @@
 */
 
 function isAnagram(str1, str2) {
-  if (str1.length != str2.length) {
+  if (str1.length !== str2.length) {
     return false;
   }
 
-  let hashMap = new Map();
+  const normalizeString = (str) => str.toLowerCase();
 
-  for (let i = 0; i < str1.length; i++) {
-    if (hashMap.has(str1[i])) {
-      hashMap.set(str1[i], hashMap.get(str1[i]) + 1);
-    } else {
-      hashMap.set(str1[i], 1);
-    }
-  }
+  const countCharacterFrequency = (str) => {
+    const charFreqMap = new Object();
 
-  for (let i = 0; i < str2.length; i++) {
-    if (hashMap.has(str2[i])) {
-      hashMap.set(str2[i], hashMap.get(str2[i]) - 1);
-    } else {
-      return false;
+    for (let char of str) {
+      charFreqMap[char] = (charFreqMap[char] || 0) + 1;
     }
-  }
+
+    return charFreqMap;
+  };
+
+  const compareFrequencyMaps = (map1, map2) => {
+    for (let key in map1) {
+      if (map1[key] !== map2[key]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const normalizedStr1 = normalizeString(str1);
+  const normalizedStr2 = normalizeString(str2);
+
+  const freqMap1 = countCharacterFrequency(normalizedStr1);
+  const freqMap2 = countCharacterFrequency(normalizedStr2);
+
+  return compareFrequencyMaps(freqMap1, freqMap2);
 }
 
 module.exports = isAnagram;
